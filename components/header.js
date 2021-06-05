8430e226230476adaa685d2bc8ed65951f678eb2
shann-singh <shann.singh@gmail.com>
2021-06-06 01:59:21 +0530

    home

---
+++ components/header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Navbar, Nav } from "react-bootstrap";
+
+const Header = () => {
+  return (
+    <Navbar bg='light' expand='lg'>
+      <Navbar.Brand href='/'>Bluestacks Full Stack Web App</Navbar.Brand>
+    </Navbar>
+  );
+};
+
+export default Header;
---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  publicRuntimeConfig: {
+    apiurl: "https://secure-forest-43886.herokuapp.com",
+  },
+};
