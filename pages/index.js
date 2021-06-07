51b235814f5b15834e2ec2f478cd3714e002ee82
shann-singh <shann.singh@gmail.com>
2021-06-08 02:46:29 +0530

    head declaration

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import Head from "next/head";
-import Image from "next/image";
 import getConfig from "next/config";
 import styles from "../styles/Home.module.css";
 import { useVideo } from "../components/videoContext";
